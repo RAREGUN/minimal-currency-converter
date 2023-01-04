59009f66e5137ce8ba8d0af2c8e24250ace13935
RAREGUN <fluxgore@gmail.com>
2023-01-04 11:59:28 +0300

    init

---
+++ minimal-currency-converter/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace minimal_currency_converter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ minimal-currency-converter/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace minimal_currency_converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
